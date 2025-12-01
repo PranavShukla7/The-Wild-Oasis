abfb9e15f316fc50cefad1b73e9c9ba70cd6e5a8
pranavshukla <shuklapranav789@gmail.com>
2025-12-01 16:44:07 +0530

    Add all changes

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://wkztpceowgpsbflkyktb.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndrenRwY2Vvd2dwc2JmbGt5a3RiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ1ODIyNDIsImV4cCI6MjA4MDE1ODI0Mn0.1AhdcB4_jO-xDtnl047ZAC33m7ztbvG3kO85JpyPU4I";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
